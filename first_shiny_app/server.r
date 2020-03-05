c581f6315078676358fbc64054998105ce4ed9c0
Andrés Del Río <andresdelrio@MacBook-Air-de-Andres.local>
2020-03-05 16:53:00 -0300

    First simple shiny app

---
+++ first_shiny_app/server.r
@@ -0,0 +1,12 @@
+
+library(shiny)
+library(ggplot2)
+
+shinyServer(function(input,output,session){
+  
+  output$HistPlot <- renderPlot({
+    ggplot(diamonds, aes_string(x=input$VarToPlot)) + geom_histogram(bins = 30)
+    
+  })
+  
+})
